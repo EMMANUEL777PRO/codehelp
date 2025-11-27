a1d0a9550398d4d91fc3fd59397191e9b5997272
Emmanuel <emmanuelmendoza13579@gmail.com>
2025-11-27 09:55:53 -0600

    configure static for render

--- codehelp/settings.py
+++ codehelp/settings.py
@@ -67,15 +67,13 @@ TEMPLATES = [
 ]
 
 # static
-import os
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
