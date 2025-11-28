e6c6d821cd944a21ba1396c50384235b7042d9db
Emmanuel <emmanuelmendoza13579@gmail.com>
2025-11-27 18:27:01 -0600

    Fix staticfiles and allowed hosts

--- codehelp/settings.py
+++ codehelp/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-g36&hy5l3wgu#x3(j6@6-n9g!udp4ds+9i!&qi*vhi4#dnu+*^
 DEBUG = False
 ALLOWED_HOSTS = [
     'codehelp-yfbw.onrender.com',
-    ]
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
@@ -72,7 +74,7 @@ TEMPLATES = [
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
