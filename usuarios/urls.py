5fc3e041ff8cff3a2184870638c86df1be79a3c8
Emmanuel <emmanuelmendoza13579@gmail.com>
2025-11-28 00:53:37 -0600

    Actualizar templates y vistas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('comentarios/', views.comentarios, name='comentarios'),
     path('tutoriales/', views.tutoriales, name='tutoriales'),
     path('contacto/', views.contacto, name='contacto'),
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+from .forms import RegistroForm
+from django.contrib.auth.forms import AuthenticationForm
 
-def home(request):
-    return render(request, 'codehelp.html')
+def registro(request):
+    if request.method == 'POST':
+        form = RegistroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Cuenta creada correctamente. Ahora puedes iniciar sesión.")
+            return redirect('login')
+    else:
+        form = RegistroForm()
+    return render(request, 'usuarios/registro.html', {'form': form})
 
-def comentarios(request):
-    return render(request, 'comentarios.html')
 
-def tutoriales(request):
-    return render(request, 'tutoriales.html')
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            usuario = form.get_user()
+            login(request, usuario)
+            messages.success(request, f"Bienvenido {usuario.username}")
+            return redirect('home')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+    else:
+        form = AuthenticationForm()
+    return render(request, 'usuarios/login.html', {'form': form})
 
-def contacto(request):
-    return render(request, 'contacto.html')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Has cerrado sesión correctamente")
+    return redirect('home')
